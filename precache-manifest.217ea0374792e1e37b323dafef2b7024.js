ff4bd279507c0a7b123935e92417d47e30f8a813
OUghTMS <misha.saan@gmail.com>
2019-07-22 10:57:31 +0300

    Updates

---
+++ precache-manifest.217ea0374792e1e37b323dafef2b7024.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e11171e2347d30c5c34157cdc04d03c",
+    "url": "/star-wars-characters/index.html"
+  },
+  {
+    "revision": "0f6c7d42020298a08da7",
+    "url": "/star-wars-characters/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/star-wars-characters/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "0f6c7d42020298a08da7",
+    "url": "/star-wars-characters/static/js/main.939dd476.chunk.js"
+  },
+  {
+    "revision": "4250787d4a54a11e5c89",
+    "url": "/star-wars-characters/static/js/runtime~main.7dadf68b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/star-wars-characters/static/media/logo.5d5d9eef.svg"
+  }
+]);
